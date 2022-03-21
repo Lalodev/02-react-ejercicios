a24bb275bbbea71c7e59dec05039cb47ca745e68
Lalodev <laloing87@gmail.com>
2022-03-21 09:46:14 -0600

    exercises done!

--- src/components/CrudApp.js
+++ src/components/CrudApp.js
@@ -1,78 +1,81 @@
-import React, { useState } from 'react';
-import CrudForm from './CrudForm';
-import CrudTable from './CrudTable';
+import React, { useState } from "react";
+import CrudForm from "./CrudForm";
+import CrudTable from "./CrudTable";
 
 const initialDb = [
-    {
-      id: 1,
-      name: "Seiya",
-      constellation: "Pegaso"
-    },
-    {
-        id: 2,
-        name: "Shiryu",
-        constellation: "Dragón"
-    },
-    {
-      id: 3,
-      name: "Hyoga",
-      constellation: "Cisne"
-    },
-    {
-      id: 4,
-      name: "Shun",
-      constellation: "Andromeda"
-    },
-    {
-      id: 5,
-      name: "Ikki",
-      constellation: "Fénix"
-    }
+  {
+    id: 1,
+    name: "Seiya",
+    constellation: "Pegaso",
+  },
+  {
+    id: 2,
+    name: "Shiryu",
+    constellation: "Dragón",
+  },
+  {
+    id: 3,
+    name: "Hyoga",
+    constellation: "Cisne",
+  },
+  {
+    id: 4,
+    name: "Shun",
+    constellation: "Andromeda",
+  },
+  {
+    id: 5,
+    name: "Ikki",
+    constellation: "Fénix",
+  },
 ];
 
 const CrudApp = () => {
-    const [db, setDb] = useState(initialDb);
-    const [dataToEdit, setDataToEdit] = useState(null);
+  const [db, setDb] = useState(initialDb);
+  const [dataToEdit, setDataToEdit] = useState(null);
 
-    const createData = (data) => {
-        data.id = Date.now();
-        //console.log(data)
-        setDb([...db, data]);
+  const createData = (data) => {
+    data.id = Date.now();
+    //console.log(data)
+    setDb([...db, data]);
+  };
+
+  const updateData = (data) => {
+    let newData = db.map((el) => (el.id === data.id ? data : el));
+    setDb(newData);
+  };
+
+  const deleteData = (id) => {
+    let isDelete = window.confirm(
+      `¿Estas seguro de eliminar el registro con el id '${id}'?`
+    );
+
+    if (isDelete) {
+      let newData = db.filter((el) => el.id !== id);
+      setDb(newData);
+    } else {
+      return;
     }
+  };
 
-    const updateData = (data) => {
-        let newData = db.map((el) => el.id === data.id ? data : el);
-        setDb(newData);
-    }
+  return (
+    <div>
+      <h2>CRUD App</h2>
+      <article className="grid-1-2">
+        <CrudForm
+          createData={createData}
+          updateData={updateData}
+          dataToEdit={dataToEdit}
+          setDataToEdit={setDataToEdit}
+        />
+        <CrudTable
+          data={db}
+          deleteData={deleteData}
+          setDataToEdit={setDataToEdit}
+        />
+      </article>
+    </div>
+  );
+};
 
-    const deleteData = (id) => {
-        let isDelete = window.confirm(`¿Estas seguro de eliminar el registro con el id '${id}'?`);
-
-        if(isDelete) {
-            let newData = db.filter(el => el.id !== id);
-            setDb(newData);
-        }else {
-            return; 
-        }
-    }
-
-    return ( 
-        <div>
-            <h2>CRUD App</h2>
-            <article className='grid-1-2'>
-                <CrudForm 
-                createData={createData} 
-                updateData={updateData} 
-                dataToEdit={dataToEdit} 
-                setDataToEdit={setDataToEdit}
-                />
-                <CrudTable 
-                    data={db} deleteData={deleteData} 
-                    setDataToEdit={setDataToEdit}
-                />
-            </article>
-        </div>
-     );
-}
- 
 export default CrudApp;
---
+++ src/components/Modals.js
@@ -0,0 +1,57 @@
+import Modal from "./Modal";
+import { useModal } from "../hooks/useModal";
+import ContactForm from "./ContactForm";
+import SongSearch from "./SongSearch";
+import ModalPortal from "./ModalPortal";
+
+const Modals = () => {
+  const [isOpenModal1, openModal1, closeModal1] = useModal(true);
+  const [isOpenModal2, openModal2, closeModal2] = useModal(false);
+  const [isOpenContact, openModalContact, closeModalContact] = useModal(false);
+  const [isOpenSong, openModalSong, closeModalSong] = useModal(false);
+
+  const [isOpenPortal, openModalPortal, closeModalPortal] = useModal(false);
+
+  return (
+    <div>
+      <h2>Modales</h2>
+      <button onClick={openModal1}>Modal 1</button>
+      <Modal isOpen={isOpenModal1} closeModal={closeModal1}>
+        <h3>Modal 1</h3>
+        <p>Hola este es el contenido del modal 1</p>
+        <img src="https://placeimg.com/400/400/animals" alt="Animals" />
+      </Modal>
+      <button onClick={openModal2}>Modal 2</button>
+      <Modal isOpen={isOpenModal2} closeModal={closeModal2}>
+        <h3>Otro Modal</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor,
+          molestias illo! Iste nisi aut dolorum nihil, similique tempore quidem
+          fugiat quia modi est. Minima suscipit veniam dolorum quisquam
+          accusamus consequatur?
+        </p>
+        <img src="https://placeimg.com/400/400/nature" alt="Nature" />
+      </Modal>
+      <button onClick={openModalContact}>Modal Contacto</button>
+      <Modal isOpen={isOpenContact} closeModal={closeModalContact}>
+        <ContactForm />
+      </Modal>
+      <button onClick={openModalSong}>Modal Canciones</button>
+      <Modal isOpen={isOpenSong} closeModal={closeModalSong}>
+        <SongSearch />
+      </Modal>
+
+      <button onClick={openModalPortal}>Modal en Portal</button>
+      <ModalPortal isOpen={isOpenPortal} closeModal={closeModalPortal}>
+        <h3>Modal en Portal</h3>
+        <p>
+          Este es el contenido de un modal que carga en otro nodo del DOM
+          diferente a donde carga nuestra app de React, gracias a un portal
+        </p>
+        <img src="https://placeimg.com/400/400/tech" alt="Tech" />
+      </ModalPortal>
+    </div>
+  );
+};
+
+export default Modals;
